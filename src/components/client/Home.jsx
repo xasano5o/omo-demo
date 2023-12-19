a58719ac983e23fc4a19e5804ab9b9ca94ee6544
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-12-19 08:59:49 +0500

    add banners

--- src/components/client/Home.jsx
+++ src/components/client/Home.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import { CategorySilide } from './CategorySilide.jsx';
 import Products from './Products';
 import Diskount from './discount/Diskount.jsx';
+import { useGetBannersQuery } from '../../redux/slice/client/banner/index.js';
 
 const Home = () => {
+    const {data}=useGetBannersQuery()
     return (
         <>
             <div className="container px-0" style={{ marginTop: "66px" }}>
                 <div id="OmoFood" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
-                        <div className="carousel-item active" data-bs-interval="10000">
-                            <img src="banners/1.png" className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item" data-bs-interval="2000">
-                            <img src="banners/2.png" className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item" data-bs-interval="2000">
-                            <img src="banners/3.png" className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item" data-bs-interval="2000">
-                            <img src="banners/4.png" className="d-block w-100" alt="..." />
-                        </div>
+                        {data?.map((item) => {
+                            return <>
+                            <div className="carousel-item active" data-bs-interval="10000">
+                                <img src={item?.image} className="d-block w-100 h-96 object-contain" alt="..." />
+                            </div>
+
+                            </>
+                        })}
+
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#OmoFood" data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
